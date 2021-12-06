754f8a1c1d4d7d471c996c1617017da9171a3941
ArulkkanthanSK <arulkkanthansk_kaliyaperumal@keybank.com>
2021-12-06 15:46:41 -0600

    Controller Method added

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	/**
+	 * The class is flagged as a @RestController, meaning it is ready for use by
+	 * Spring MVC to handle web requests. @GetMapping maps / to the index() method.
+	 * When invoked from a browser or by using curl on the command line, the method
+	 * returns pure text. That is because @RestController combines @Controller
+	 * and @ResponseBody, two annotations that results in web requests returning
+	 * data rather than a view.
+	 * 
+	 * @return
+	 */
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
