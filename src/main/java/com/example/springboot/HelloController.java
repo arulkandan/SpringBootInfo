4ef24e1e7ee80ac3fe03581444c9fd8fa8331173
ArulkkanthanSK <arulkkanthansk_kaliyaperumal@keybank.com>
2021-12-06 23:21:03 -0600

    Swagger Advanced Annotation

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,7 +1,19 @@
 package com.example.springboot;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.model.Hello;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 public class HelloController {
@@ -19,6 +31,17 @@ public class HelloController {
 	@GetMapping("/index")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+	
+	@ApiOperation(value = "getGreeting", nickname = "getGreeting")
+	 @ApiResponses(value = {
+		        @ApiResponse(code = 500, message = "Server error"),
+		        @ApiResponse(code = 200, message = "Successful retrieval",
+		            response = Hello.class) })
+	@GetMapping("/index/{institutuionId}")
+	public Hello sayHello(@ApiParam(value = "testId",
+	        required = true, defaultValue = "111")  @PathVariable final int institutuionId) {
+		return new Hello();
 	}
 
 }
